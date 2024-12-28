5c26bdcb11d9a8d4b5e639045e69e0ea269a0e50
learning Git <sachinkumar270702@gmail.com>
2024-12-28 16:43:28 +0530

    new elements

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import { createRoot } from 'react-dom/client'
+
+
+const root=createRoot(document.getElementById('root'))
+root.render(
+  <main>
+  <img src="/src/assets/react-logo.png" width="40px" alt ="React Logo"/>
+  <h1>Fun fact about react</h1>
+  <ul>
+    <li>was first released in 2013</li>
+    <li>Was originally created by Jordan walke</li>
+    <li>Has well over 200k stars on Github</li>
+    <li>Is mainetained by Meta</li>
+    <li>Powers thousands of enterprices apps, including mobile apps</li>
+  </ul>
+  </main> 
 )
