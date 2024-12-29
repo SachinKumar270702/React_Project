cd48b24116bc3c9847dd269796fbc6e6465d3e97
learning Git <sachinkumar270702@gmail.com>
2024-12-29 15:24:25 +0530

    craeting page

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,41 @@
 
 import { createRoot } from 'react-dom/client'
+import { Fragment } from 'react'
+import Header from './Header'
 
 
-const root=createRoot(document.getElementById('root'))
+const root = createRoot(document.getElementById('root'))
 root.render(
-  <main>
-  <img src="/src/assets/react-logo.png" width="40px" alt ="React Logo"/>
-  <h1>Fun fact about react</h1>
-  <ul>
-    <li>was first released in 2013</li>
-    <li>Was originally created by Jordan walke</li>
-    <li>Has well over 200k stars on Github</li>
-    <li>Is mainetained by Meta</li>
-    <li>Powers thousands of enterprices apps, including mobile apps</li>
-  </ul>
-  </main> 
+  <Page />
 )
+
+function MainContent() {
+  return (
+    <div className="content">
+
+      <h1>Reasons I'm Excited to learn React</h1>
+      <ol className="list-content">
+        <li className='list-content-items'>im Sachin kumar, im so exited to be here because i wanted to learn raect from a very long time</li>
+        <li className='list-content-items'>I like to learn react cause it is highly damamneded lag.</li>
+        <li className='list-content-items'>i would like to create more things in React</li>
+      </ol>
+    </div>
+  )
+}
+
+function Footer(){
+  return(
+    <footer className='footer-content'>
+      <small>© 2024 kumar development.All rights reserved.</small>
+    </footer>
+  )
+}
+function Page() {
+  return (
+    <>
+      <Header />
+      <MainContent />
+      <Footer />
+    </>
+  )
+}
